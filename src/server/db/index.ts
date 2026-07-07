77ddb37ff0f15882a474aba8c64ea037b8a965e0
Sloan Finger <hello@sloanfinger.com>
2026-07-07 15:39:20 -0400

    remove connection pooling

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,28 +1,31 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import { createPool, type Pool } from "mysql2/promise";
+import {
+  createConnection,
+  type Connection
+} from "mysql2/promise";
 import { env } from "~/env";
-import { schema, relations } from "./schema";
+import { relations, schema } from "./schema";
 
 async function getDB() {
   const globalForDb = globalThis as unknown as {
-    pool: Pool | undefined;
+    connection: Connection | undefined;
   };
 
   const client =
-    globalForDb.pool ??
-    createPool({
+    globalForDb.connection ??
+    (await createConnection({
       host: env.MYSQL_HOST,
       user: env.MYSQL_USER,
       password: env.MYSQL_PASSWORD,
       port: env.MYSQL_PORT,
       database: env.MYSQL_DATABASE,
       disableEval: true,
-    });
+    }));
 
   const db = drizzle({ client, schema, relations, mode: "default" });
 
   if (env.NODE_ENV !== "production") {
-    globalForDb.pool = client;
+    globalForDb.connection = client;
   }
 
   return db;
